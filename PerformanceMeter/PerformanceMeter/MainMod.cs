2f87895d850052bc522f8c8110eaec8dcb465a20
Ryan Fredlund <rfredlund13@gmail.com>
2022-03-12 19:43:12 -0600

    Playing with showing percentage and seeing the game object hierarchy

--- PerformanceMeter/PerformanceMeter/MainMod.cs
+++ PerformanceMeter/PerformanceMeter/MainMod.cs
@@ -10,9 +10,11 @@ namespace PerformanceMeter
 {
     public class MainMod : MelonMod
     {
-        private static String STAGE_SCENE_NAME = "03.Stage";
+        private static string SCENE_NAME_STAGE = "03.Stage";
+        private static string SCENE_NAME_GAME_END = "3.GameEnd";
 
         public static MelonPreferences_Category prefs;
+        public static Util util;
 
         private bool inStage = false;
         private bool failed = false;
@@ -32,27 +34,125 @@ namespace PerformanceMeter
             prefs = MelonPreferences.CreateCategory("MainPreferences", "Preferences");
             prefs.SetFilePath("UserData/PerformanceMeter.cfg");
 
+            util = new Util(LoggerInstance);
             lifePctFrames = new List<float>();
         }
 
         private void reset()
         {
-            LoggerInstance.Msg("Reset");
             failed = false;
+            inStage = false;
             lifePctFrames.Clear();
+        }
+
+        private void injectAverageLifePctText(Game_ScoreSceneController scoreSceneController, float avgLifePct)
+        {
+            TMPro.TMP_Text totalScoreTextObject = scoreSceneController.totalScore;
+
+            // TODO clean up finding
+            /*
+             * [13:19:51.469] [Performance_Meter] Reference game object: Value (TMPro.TextMeshPro)
+[13:19:51.470] [Performance_Meter] Parent exists: TotalScore (UnityEngine.Transform)
+[13:19:51.471] [Performance_Meter]     Name: TotalScore
+[13:19:51.471] [Performance_Meter]     GO: TotalScore (UnityEngine.GameObject)
+[13:19:51.472] [Performance_Meter] Parent exists: ScoreWrap (UnityEngine.Transform)
+[13:19:51.473] [Performance_Meter]     Name: ScoreWrap
+[13:19:51.474] [Performance_Meter]     GO: ScoreWrap (UnityEngine.GameObject)
+[13:19:51.475] [Performance_Meter] Parent exists: No Multiplayer (UnityEngine.Transform)
+[13:19:51.477] [Performance_Meter]     Name: No Multiplayer
+[13:19:51.478] [Performance_Meter]     GO: No Multiplayer (UnityEngine.GameObject)
+[13:19:51.479] [Performance_Meter] Parent exists: DisplayWrap (UnityEngine.Transform)
+[13:19:51.480] [Performance_Meter]     Name: DisplayWrap
+[13:19:51.481] [Performance_Meter]     GO: DisplayWrap (UnityEngine.GameObject)
+[13:19:51.482] [Performance_Meter] Parent exists: [Game_Scripts] (UnityEngine.Transform)
+[13:19:51.482] [Performance_Meter]     Name: [Game_Scripts]
+[13:19:51.483] [Performance_Meter]     GO: [Game_Scripts] (UnityEngine.GameObject)
+[13:19:51.484] [Performance_Meter] Top parent: [Game_Scripts] . [Game_Scripts] (UnityEngine.GameObject)
+             */
+
+            // Works but overlaps
+            /*
+            Transform totalScoreTransform = totalScoreTextObject.transform.parent;
+            Transform scoreWrapTransform = totalScoreTransform.parent;
+            LoggerInstance.Msg("scorewrap children: " + scoreWrapTransform.childCount);
+
+            GameObject duplicateText = GameObject.Instantiate(totalScoreTransform.gameObject, scoreWrapTransform);
+            duplicateText.name = "pmAvgLifePct";
+            duplicateText.GetComponentInChildren<TMPro.TMP_Text>().text = string.Format("Average life percentage: {0:0.##}", avgLifePct);
+            */
+
+            Transform totalScoreTransform = totalScoreTextObject.transform.parent;
+            LoggerInstance.Msg("total score children: " + totalScoreTransform.childCount);
+
+            GameObject duplicateText = GameObject.Instantiate(totalScoreTransform.gameObject, totalScoreTransform);
+
+            string labelChildName = "Label";
+            string valueChildName = "Value";
+
+            int numChildren = duplicateText.transform.childCount;
+            for (int i = numChildren - 1; i >= 0; i--)
+            {
+                Transform child = duplicateText.transform.GetChild(i);
+                if (child.name == labelChildName)
+                {
+                    util.SetTMProText(child, "Avg life pct: ");
+                }
+                else if (child.name == valueChildName)
+                {
+                    util.SetTMProText(child, string.Format("{0:0.##}", avgLifePct));
+                }
+                else
+                {
+                    LoggerInstance.Msg("Removing child " + child);
+                    GameObject.Destroy(child.gameObject);
+                }
+            }
+
+            duplicateText.name = "pmAvgLifePct";
+            RectTransform[] rects = duplicateText.GetComponentsInChildren<RectTransform>();
+            LoggerInstance.Msg("Rects: " + rects?.Length);
+            LoggerInstance.Msg("Transform: " + duplicateText.transform.localPosition);
+
+            util.LogGameObjectHierarchy(util.GetRootTransform(totalScoreTransform));
+
+            /*
+            Transform topParent = totalScoreTextObject.transform;
+            while (topParent.parent != null)
+            {
+                LoggerInstance.Msg("Parent exists: " + topParent.parent);
+                LoggerInstance.Msg("    Name: " + topParent.parent.name);
+                LoggerInstance.Msg("    GO: " + topParent.parent.gameObject);
+                topParent = topParent.parent;
+            }
+            LoggerInstance.Msg("Top parent: " + topParent.name + " . " + topParent.gameObject);
+            */
+
+            // avgLifePctText.SetText(string.Format("{0:0.##}", avgLifePct));
         }
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             base.OnSceneWasLoaded(buildIndex, sceneName);
 
-            if (lifePctFrames.Count > 0)
+            if (sceneName == SCENE_NAME_GAME_END)
             {
-                LoggerInstance.Msg(lifePctFrames.Count + " frames recorded.");
+                if (lifePctFrames.Count > 0)
+                {
+                    LoggerInstance.Msg(lifePctFrames.Count + " frames recorded.");
+                    float avgLifePct = lifePctFrames.Average();
+                    LoggerInstance.Msg("Average life pct: " + avgLifePct);
+
+                    Game_ScoreSceneController scoreSceneController = Game_ScoreSceneController.s_instance;
+                    if (scoreSceneController != null)
+                    {
+                        injectAverageLifePctText(scoreSceneController, avgLifePct);
+                    }
+                }
             }
 
             reset();
-            inStage = (sceneName == STAGE_SCENE_NAME);
+
+            inStage = sceneName == SCENE_NAME_STAGE;
 
             LoggerInstance.Msg("Scene: " + sceneName + ", stage? " + inStage);
         }
@@ -86,12 +186,15 @@ namespace PerformanceMeter
 
         public override void OnUpdate()
         {
-            if (inStage && !failed && shouldCheckLifePct())
+            if (inStage && !failed)
             {
-                float lifePct = Synth.Utils.LifeBarHelper.GetScalePercent();
-                
-                lifePctFrames.Add(lifePct);
-                failed = lifePct <= 0;
+                if (shouldCheckLifePct())
+                {
+                    float lifePct = Synth.Utils.LifeBarHelper.GetScalePercent();
+
+                    lifePctFrames.Add(lifePct);
+                    failed = lifePct <= 0;
+                }
             }
         }
     }
---
+++ PerformanceMeter/PerformanceMeter/Util.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+namespace PerformanceMeter
+{
+    public class Util
+    {
+        private MelonLogger.Instance logger;
+
+        public Util(MelonLogger.Instance logger)
+        {
+            this.logger = logger;
+        }
+
+        /**
+         * Retrieve the root (most parent) transform from the given transform.
+         */
+        public Transform GetRootTransform(Transform transform)
+        {
+            if (transform == null)
+            {
+                return null;
+            }
+
+            Transform currentTransform = transform;
+            while (currentTransform.parent != null)
+            {
+                currentTransform = currentTransform.parent;
+            }
+
+            return currentTransform;
+        }
+
+        /**
+         * Prints out the game object tree/hierarchy below the given root object to the logs.
+         * Useful for finding objects to clone/instantiate :)
+         */
+        public void LogGameObjectHierarchy(Transform root, int indentLevel = 0)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            string tabs = "";
+            for (int i = 0; i < indentLevel; i++)
+            {
+                tabs += "\t";
+            }
+
+            // Root
+            logger.Msg(string.Format(
+                "{0}{1} at local position {2} (global position {3}) with rotation {4}",
+                tabs,
+                root.name,
+                root.localPosition,
+                root.position,
+                root.rotation
+            ));
+
+            // Children
+            for (int i = 0; i < root.childCount; i++)
+            {
+                LogGameObjectHierarchy(root.GetChild(i), indentLevel++);
+            }
+        }
+
+        public void SetTMProText(Transform parent, string text)
+        {
+            if (parent == null)
+            {
+                return;
+            }
+
+            TMPro.TMP_Text textComponent = parent.GetComponent<TMPro.TMP_Text>();
+            if (textComponent != null)
+            {
+                textComponent.text = text;
+            }
+        }
+    }
+}
