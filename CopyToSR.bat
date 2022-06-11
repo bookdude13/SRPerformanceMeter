e7541ce32896c09558d082620945abf336adad11
Ryan Fredlund <rfredlund13@gmail.com>
2022-06-11 17:26:22 -0500

    Update scripts to match other projects

--- CopyToSR.bat
+++ CopyToSR.bat
@@ -1,7 +1,8 @@
+set MOD_NAME="PerformanceMeter"
 
-set BUILT_DLL=".\PerformanceMeter\PerformanceMeter\bin\Debug\PerformanceMeter.dll"
-set DB_DLL=".\PerformanceMeter\PerformanceMeter\bin\Debug\LiteDB.dll"
-set LIB_DLL_DIR=".\PerformanceMeter\PerformanceMeter\bin\Debug\libs"
+set BUILT_DLL=".\%MOD_NAME%\%MOD_NAME%\bin\Debug\%MOD_NAME%.dll"
+set DB_DLL=".\%MOD_NAME%\%MOD_NAME%\bin\Debug\LiteDB.dll"
+set LIB_DLL_DIR=".\%MOD_NAME%\%MOD_NAME%\bin\Debug\libs"
 set SYNTHRIDERS_MODS_DIR="C:\Program Files (x86)\Steam\steamapps\common\SynthRiders\Mods"
 
 copy %BUILT_DLL% %SYNTHRIDERS_MODS_DIR%
